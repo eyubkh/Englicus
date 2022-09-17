ef46e19235726268b91cf36098b74d4a50bbf6da
eyubkh <eyub.kh@gmail.com>
2022-09-17 19:50:30 +0100

    build: adding tokens

---
+++ tokens/build/js/_variables.js
@@ -0,0 +1,59 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 17 Sep 2022 18:30:13 GMT
+ */
+
+export const BrandPrimary = "#faee7d";
+export const BrandSecondary = "#7f8bfa";
+export const ActionSuccess100 = "#86fa7d";
+export const ActionError100 = "#fa827d";
+export const ActionSuccess300 = "#0d8205";
+export const ActionError300 = "#9c0b06";
+export const ActionSuccess200 = "#12b607";
+export const ActionError200 = "#d00e08";
+export const NeutralLight100 = "#ffffff";
+export const NeutralLight200 = "#f8f8f8";
+export const NeutralLight300 = "#f0f0f0";
+export const NeutralGrey100 = "#838383";
+export const NeutralGrey200 = "#6d6d6d";
+export const NeutralGrey300 = "#575757";
+export const NeutralDark300 = "#2c2c2c";
+export const NeutralDark200 = "#161616";
+export const NeutralDark100 = "#000000";
+export const FontFamiliesRoundedMplus1c = "Rounded Mplus 1c";
+export const LineHeights0 = "110%";
+export const LineHeights1 = "120%";
+export const LineHeights2 = "130%";
+export const LineHeights3 = "150%";
+export const LineHeights4 = "160%";
+export const LineHeights5 = "180%";
+export const FontWeightsRoundedMplus1c0 = "ExtraBold";
+export const FontWeightsRoundedMplus1c1 = "Regular";
+export const FontSize0 = "12px";
+export const FontSize1 = "17px";
+export const FontSize2 = "28px";
+export const FontSize3 = "44px";
+export const FontSize4 = "72px";
+export const FontSize5 = "116px";
+export const LetterSpacing0 = "-2%";
+export const LetterSpacing1 = "-1%";
+export const LetterSpacing2 = "0%";
+export const LetterSpacing3 = "5%";
+export const ParagraphSpacing0 = "0";
+export const H1 = {"fontFamily":"Rounded Mplus 1c","fontWeight":"ExtraBold","lineHeight":"110%","fontSize":"116px","letterSpacing":"-2%","paragraphSpacing":"0","textCase":"none","textDecoration":"none"};
+export const H2 = {"fontFamily":"Rounded Mplus 1c","fontWeight":"ExtraBold","lineHeight":"120%","fontSize":"72px","letterSpacing":"-1%","paragraphSpacing":"0","textCase":"none","textDecoration":"none"};
+export const H3 = {"fontFamily":"Rounded Mplus 1c","fontWeight":"ExtraBold","lineHeight":"130%","fontSize":"44px","letterSpacing":"0%","paragraphSpacing":"0","textCase":"none","textDecoration":"none"};
+export const H4 = {"fontFamily":"Rounded Mplus 1c","fontWeight":"ExtraBold","lineHeight":"150%","fontSize":"28px","letterSpacing":"0%","paragraphSpacing":"0","textCase":"none","textDecoration":"none"};
+export const BodyTextRegular = {"fontFamily":"Rounded Mplus 1c","fontWeight":"Regular","lineHeight":"160%","fontSize":"17px","letterSpacing":"0%","paragraphSpacing":"0","textCase":"none","textDecoration":"none"};
+export const BodyTextLarge = {"fontFamily":"Rounded Mplus 1c","fontWeight":"Regular","lineHeight":"150%","fontSize":"28px","letterSpacing":"0%","paragraphSpacing":"0","textCase":"none","textDecoration":"none"};
+export const BodyTextSmall = {"fontFamily":"Rounded Mplus 1c","fontWeight":"Regular","lineHeight":"180%","fontSize":"12px","letterSpacing":"0%","paragraphSpacing":"0","textCase":"none","textDecoration":"none"};
+export const BodyTextRegularBold = {"fontFamily":"Rounded Mplus 1c","fontWeight":"ExtraBold","lineHeight":"160%","fontSize":"17px","letterSpacing":"5%","paragraphSpacing":"0","textCase":"none","textDecoration":"none"};
+export const SpacingColor = "#c4c4c4";
+export const TextCaseNone = "none";
+export const TextDecorationNone = "none";
+export const Spacing0 = "16px";
+export const Spacing1 = "24px";
+export const Spacing2 = "32px";
+export const Spacing3 = "56px";
+export const Spacing4 = "96px";
+export const Radius0 = "8px";
