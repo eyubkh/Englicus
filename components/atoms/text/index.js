3da9f687b4dfb6c0b7225ed883471cdd4dc8adb1
eyubkh <eyub.kh@gmail.com>
2022-10-02 16:02:44 +0100

    build: deleting unused components

--- components/atoms/text/index.js
+++
@@ -1,5 +0,0 @@
-export { DisplayText } from './DisplayText'
-export { Heading } from './Heading'
-export { TextField } from './TextField'
-export { TextStyle } from './TextStyle'
-export { TextContainer } from './TextContainer'
