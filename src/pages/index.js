49cbefb305726a60573e60731f1cebb063504283
eyubkh <eyub.kh@gmail.com>
2022-10-02 18:32:01 +0100

    build: adding logo to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import { Button } from '@components/atoms/Button'
-import {} from '@tokens'
+import { Logo } from '@components/atoms/Logo'
 
 export default function Home () {
   return (
     <>
       <h1>Hello world!!!</h1>
       <Button>click</Button>
+      <Logo />
     </>
   )
 }
