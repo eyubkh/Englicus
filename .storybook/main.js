3f1913b9bfd1b95d8e0d6cf87a569f92d82095af
eyubkh <eyub.kh@gmail.com>
2022-10-02 10:06:09 +0100

    build: adding webpack aliases

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,26 @@
+const path = require("path");
+
 module.exports = {
-    stories: [
-        '../storybook/**/*.stories.mdx',
-        '../storybook/**/*.stories.@(js|jsx|ts|tsx)',
-    ],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/addon-interactions',
-    ],
-    framework: '@storybook/react',
-    core: {
-        builder: '@storybook/builder-webpack5',
-    },
-}
+  stories: [
+    "../storybook/**/*.stories.mdx",
+    "../storybook/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    config.resolve.alias = {
+      "@components": path.resolve(__dirname, "../components"),
+      "@tokens": path.resolve(__dirname, "../tokens/build/js/_variables.js"),
+    };
+
+    return config;
+  },
+};
