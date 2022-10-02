5c89839f3288d392dedb12c6e9763b0907158cff
eyubkh <eyub.kh@gmail.com>
2022-10-02 20:48:36 +0100

    build: adding Cross Icon component

---
+++ storybook/atoms/CrossIcon.stories.jsx
@@ -0,0 +1,8 @@
+import { CrossIcon as Component } from '@components/atoms/CrossIcon'
+
+export default {
+  title: 'UI Web/atoms',
+  component: Component
+}
+
+export const CrossIcon = () => <CrossIcon />
