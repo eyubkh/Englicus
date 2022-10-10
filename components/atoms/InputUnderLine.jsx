935ec06b1dc0a2e43ce20c513da0f9c9ed35fa34
eyubkh <eyub.kh@gmail.com>
2022-10-10 11:30:41 +0100

    build: adding styles and changing placeholder

--- components/atoms/InputUnderLine.jsx
+++ components/atoms/InputUnderLine.jsx
@@ -1,11 +1,18 @@
-import { BrandSecondary } from '@tokens'
+import { BodyH3, Border0, BrandSecondary, Spacing0, Spacing1 } from '@tokens'
 import styled from 'styled-components'
 
 const InputComponent = styled.input`
-    border-color: ${BrandSecondary};
-    border-bottom: solid black 3px;
+  ${BodyH3}
+    border-bottom: ${Border0} solid ${BrandSecondary};
+    border-top: none;
+    border-right: none;
+    border-left: none;
+    text-align: center;
+    padding: ${Spacing0} ${Spacing1};
+    max-width: 300px;
+    outline: none;
 `
 
 export const InputUnderLine = () => {
-  return <InputComponent type='text' placeholder='Haaaaaa' />
+  return <InputComponent type='text' placeholder='Text' />
 }
