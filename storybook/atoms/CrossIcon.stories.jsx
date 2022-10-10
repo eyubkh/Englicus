07703cb3a7daed98456f8c8aad24d42181b4f48f
eyubkh <eyub.kh@gmail.com>
2022-10-10 11:20:55 +0100

    build: renaming storybook folder to stories

--- storybook/atoms/CrossIcon.stories.jsx
+++
@@ -1,8 +0,0 @@
-import { CrossIcon as Component } from '@components/atoms/CrossIcon'
-
-export default {
-  title: 'UI Web/atoms',
-  component: Component
-}
-
-export const CrossIcon = () => <CrossIcon />
--- storybook/atoms/Logo.stories.jsx
+++
@@ -1,8 +0,0 @@
-import { Logo as Component } from '@components/atoms/Logo'
-
-export default {
-  title: 'UI Web/atoms',
-  component: Component
-}
-
-export const Logo = () => <Component />
