ced28433fbb0817ad680b810aad92da0f11824aa
eyubkh <eyub.kh@gmail.com>
2022-10-10 11:27:24 +0100

    test: adding style checks

--- __test__/atoms/InputUnderLine.test.jsx
+++ __test__/atoms/InputUnderLine.test.jsx
@@ -1,23 +1,35 @@
 import { render, screen } from '@testing-library/react'
 import { InputUnderLine } from '@components/atoms/InputUnderLine'
-import { BrandSecondary } from '@tokens'
+import { BodyH3, Border0, BrandSecondary } from '@tokens'
+
+let component
 
 beforeEach(() => {
   render(<InputUnderLine />)
+  component = screen.getByRole('textbox')
 })
 
-describe('Input is rendering', () => {
+describe('Component is rendering', () => {
   test('is on screen', () => {
-    expect(screen.getByRole('textbox')).toBeDefined()
+    expect(component).toBeDefined()
   })
 })
 
-describe('Input style', () => {
-  test('has border color as secondary brand color', () => {
-    expect(screen.getByRole('textbox')).toHaveStyle(`border-color: ${BrandSecondary}`)
-  })
-
+describe('Component style', () => {
   test('has border bottom', () => {
-    expect(screen.getByRole('textbox')).toHaveStyle('border-bottom: 3px')
+    expect(component).toHaveStyle(`border-bottom: ${Border0} solid ${BrandSecondary}`)
+  })
+  test('has not border top', () => {
+    expect(component).toHaveStyle('border-top: none')
+  })
+  test('has not border right', () => {
+    expect(component).toHaveStyle('border-right: none')
+  })
+  test('has not border left', () => {
+    expect(component).toHaveStyle('border-left: none')
+  })
+  test('has text h3', () => {
+    const stylesH3 = Object.entries(BodyH3).map(style => `${style[0]}: ${style[1]}`)
+    expect(component).toHaveStyle(...stylesH3)
   })
 })
