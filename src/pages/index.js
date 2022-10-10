37c3cbb60407c28f68fafb5d7d100dae44f7ed28
eyubkh <eyub.kh@gmail.com>
2022-10-10 14:16:39 +0100

    build: test succesbutton

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import { Button } from '@components/atoms/Button'
-import { Logo } from '@components/atoms/Logo'
+import { SuccessButton } from '@components/molecules/SuccessButton'
 
 export default function Home () {
   return (
     <>
       <h1>Hello world!!!</h1>
       <Button>click</Button>
-      <Logo />
+      <SuccessButton>Sampleeee</SuccessButton>
     </>
   )
 }
