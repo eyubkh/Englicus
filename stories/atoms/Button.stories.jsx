b1d375d52256a6e84c49d2be7e4ae856187a5db9
eyubkh <eyub.kh@gmail.com>
2022-10-10 11:22:51 +0100

    style: changing stories to folder stories

---
+++ stories/atoms/Button.stories.jsx
@@ -0,0 +1,8 @@
+import { Button as Component } from '@components/atoms/Button'
+
+export default {
+  title: 'UI Web/atoms',
+  component: Component
+}
+
+export const Button = () => <Component>click here!</Component>
---
+++ stories/atoms/InputUnderLine.stories.jsx
@@ -0,0 +1,8 @@
+import { InputUnderLine as Component } from '@components/atoms/InputUnderLine'
+
+export default {
+  title: 'UI Web/atoms',
+  component: Component
+}
+
+export const InputUnderLine = () => <Component />
