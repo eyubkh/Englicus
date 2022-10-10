74b577a8f92381f73a65ca1f9e7b2acfdf6c5a47
eyubkh <eyub.kh@gmail.com>
2022-10-10 18:27:00 +0100

    build: changing logo to h4

--- components/atoms/Logo.jsx
+++ components/atoms/Logo.jsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
-import { BrandPrimary, NeutralLight100, BodyH3 } from '@tokens'
+import { BrandPrimary, NeutralLight100, BodyH4 } from '@tokens'
 
-const LogoComponent = styled.p`
-  ${BodyH3}
+const LogoComponent = styled.h4`
+  ${BodyH4}
   text-transform: uppercase;
   color: ${NeutralLight100};
   span {
