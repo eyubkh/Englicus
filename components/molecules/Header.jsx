723be5b9711807dc3f63ad46aed1b5a0ae5b9551
eyubkh <eyub.kh@gmail.com>
2022-10-26 11:50:35 +0100

    build: changing css

--- components/molecules/Header.jsx
+++ components/molecules/Header.jsx
@@ -3,10 +3,14 @@ import { NeutralDark300 } from '@tokens'
 import styled from 'styled-components'
 
 const HeaderComponent = styled.header`
-    display: flex;
-    align-items: center;
-    background-color: ${NeutralDark300};
-    padding-left: 10%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  background-color: ${NeutralDark300};
+  padding-left: 10%;
 `
 
 export const Header = () => {
