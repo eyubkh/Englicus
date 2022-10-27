fba98bb878606ec990ed66cae6badb81c81cd8db
eyubkh <eyub.kh@gmail.com>
2022-10-27 11:25:25 +0100

    build: fixing game layout card crash

--- stories/molecules/GameLayoutCard.stories.jsx
+++ stories/molecules/GameLayoutCard.stories.jsx
@@ -1,9 +1,8 @@
 import { GameLayoutCard as Component } from '@components/molecules/GameLayoutCard'
-import Svg from '@assets/FillTheGaps.svg'
 
 export default {
   title: 'UI Web/molecules',
   component: Component
 }
 
-export const GameLayoutCard = () => <Component src={Svg}>Sample text</Component>
+export const GameLayoutCard = () => <Component href='#'>Sample text</Component>
