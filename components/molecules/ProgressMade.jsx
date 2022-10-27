39fe8827e54ede89c32281c6b8f2894065fcd1e9
eyubkh <eyub.kh@gmail.com>
2022-10-27 17:55:01 +0100

    build: adding progress made component

---
+++ components/molecules/ProgressMade.jsx
@@ -0,0 +1,63 @@
+
+import { NeutralGrey100 } from '@tokens'
+import styled from 'styled-components'
+import { CrossedProgressBar } from './CrossedProgressBar'
+
+const ProgressMadeComponent = styled.div`
+    transform: rotate(90deg) translateX(50%);
+    position: relative;
+    width: 550px;
+
+    span {
+      transform: rotate(-90deg);
+      position: absolute;
+      left: 15%;
+      top: 95px;
+    }
+    .block1 {
+      position: absolute;
+      left: 20%;
+      top: -20px;
+      height: 20px;
+      width: 10px;
+      background-color: ${NeutralGrey100};
+    }
+    .block2 {
+      position: absolute;
+      left: 20%;
+      top: -60px;
+      height: 20px;
+      width: 10px;
+      background-color: ${NeutralGrey100};
+    }
+    .block3 {
+      position: absolute;
+      left: 20%;
+      top: 60px;
+      height: 20px;
+      width: 10px;
+      background-color: ${NeutralGrey100};
+    }
+    .block4 {
+      position: absolute;
+      left: 20%;
+      top: 100px;
+      height: 20px;
+      width: 10px;
+      background-color: ${NeutralGrey100};
+    }
+    
+`
+
+export const ProgressMade = () => {
+  return (
+    <ProgressMadeComponent>
+      <span>min</span>
+      <div className='block1' />
+      <div className='block2' />
+      <div className='block3' />
+      <div className='block4' />
+      <CrossedProgressBar href='#' progress={30} />
+    </ProgressMadeComponent>
+  )
+}
---
+++ stories/molecules/ProgressMade.stories.jsx
@@ -0,0 +1,8 @@
+import { ProgressMade as Component } from '@components/molecules/ProgressMade'
+
+export default {
+  title: 'UI Web/molecules',
+  component: Component
+}
+
+export const ProgressMade = () => <Component />
