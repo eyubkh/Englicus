5b481cf5dd90b63f1c185b20fb7cbb73bd22c510
eyubkh <eyub.kh@gmail.com>
2022-10-27 11:23:53 +0100

    build: adding irregular verbs section component

---
+++ components/organisms/IrregularVerbs.jsx
@@ -0,0 +1,69 @@
+import { GameTitle } from '@components/atoms/GameTitle'
+import { GameLayoutCard } from '@components/molecules/GameLayoutCard'
+import styled from 'styled-components'
+import FillTheGapsSvg from '@public/assets/FillTheGaps.svg'
+
+const IrregularVerbsSection = styled.section`
+  max-width: 1200px;
+  margin: 0 auto;
+  width: 100%;
+
+  article {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+  }
+
+`
+
+export const IrregurlarVerbs = () => {
+  return (
+    <IrregularVerbsSection>
+      <GameTitle>IrregularVerbs</GameTitle>
+      <article>
+        <GameLayoutCard
+          href='/irregularVerbs/FillTheGaps'
+          src={FillTheGapsSvg}
+          alt='fill the gaps image'
+        >
+          Fill the gaps
+        </GameLayoutCard>
+        <GameLayoutCard
+          href='#'
+          src={FillTheGapsSvg}
+          alt='fill the gaps image'
+        >
+          Sample
+        </GameLayoutCard>
+        <GameLayoutCard
+          href='#'
+          src={FillTheGapsSvg}
+          alt='Sample image'
+        >
+          Sample
+        </GameLayoutCard>
+        <GameLayoutCard
+          href='#'
+          src={FillTheGapsSvg}
+          alt='Sample image'
+        >
+          Sample
+        </GameLayoutCard>
+        <GameLayoutCard
+          href='#'
+          src={FillTheGapsSvg}
+          alt='Sample image'
+        >
+          Sample
+        </GameLayoutCard>
+        <GameLayoutCard
+          href='#'
+          src={FillTheGapsSvg}
+          alt='Sample image'
+        >
+          Sample
+        </GameLayoutCard>
+      </article>
+    </IrregularVerbsSection>
+  )
+}
---
+++ stories/organisms/IrregularVerbs.stories.js
@@ -0,0 +1,8 @@
+import { IrregurlarVerbs as Component } from '@components/organisms/IrregularVerbs'
+
+export default {
+  title: 'UI Web/organisms',
+  component: Component
+}
+
+export const IrregularVerbs = () => <Component />
