43ddf573baec241e7299c1518a0580903bd1e842
eyubkh <eyub.kh@gmail.com>
2022-10-27 11:24:33 +0100

    build: using irregular verbs component

--- components/pages/Learn.jsx
+++ components/pages/Learn.jsx
@@ -1,12 +1,9 @@
-import { GameTitle } from '@components/atoms/GameTitle'
-import { GameLayoutCard } from '@components/molecules/GameLayoutCard'
 import { Header } from '@components/molecules/Header'
-import Link from 'next/link'
 import styled from 'styled-components'
-import FillTheGapsImage from '@assets/FillTheGaps.svg'
+import { IrregurlarVerbs } from '@components/organisms/IrregularVerbs'
 
 const MainComponent = styled.main`
-  padding-top: 117px;
+  margin-top: 167px;
   padding: 0px 10%;
 `
 
@@ -14,16 +11,8 @@ export const Learn = () => {
   return (
     <MainComponent>
       <Header />
-      <h1>Welcome to Learn page!</h1>
-      <GameTitle>Irregular verbs</GameTitle>
-      <GameLayoutCard
-        href='/irregularVerbs/FillTheGaps'
-        src={FillTheGapsImage}
-        alt='fill the gaps image'
-      >
-        Fill the gaps
-      </GameLayoutCard>
-      <Link href='/'>Go back</Link>
+      <IrregurlarVerbs />
+      <IrregurlarVerbs />
     </MainComponent>
   )
 }
