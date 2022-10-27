7ffbbba56728addec2521101f5f176dd031238a1
eyubkh <eyub.kh@gmail.com>
2022-10-27 11:22:36 +0100

    build: adding link to the logo

--- components/atoms/Logo.jsx
+++ components/atoms/Logo.jsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
 import { BrandPrimary, NeutralLight100, BodyH4 } from '@tokens'
+import Link from 'next/link'
 
 const LogoComponent = styled.h4`
   ${BodyH4}
+  cursor: pointer;
   text-transform: uppercase;
   color: ${NeutralLight100};
   span {
@@ -11,5 +13,9 @@ const LogoComponent = styled.h4`
 `
 
 export const Logo = () => {
-  return <LogoComponent><span>eng</span>licus</LogoComponent>
+  return (
+    <Link href='/'>
+      <LogoComponent><span>eng</span>licus</LogoComponent>
+    </Link>
+  )
 }
