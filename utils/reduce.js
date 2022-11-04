05004ad2594e08050a82387f7851ea3a919e007c
eyubkh <eyub.kh@gmail.com>
2022-11-04 18:18:52 +0000

    build: adding reduce and context

---
+++ utils/reduce.js
@@ -0,0 +1,51 @@
+export const initialState = {
+  current: 0,
+  isCorrect: false,
+  isChecking: false,
+  isLoading: true,
+  progress: 0,
+  api: [],
+  textField: ''
+}
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case 'progress':
+      return {
+        ...state,
+        progress: state.progress + 1
+      }
+    case 'current':
+      return {
+        ...state,
+        current: state.current + 1
+      }
+    case 'isCorrect':
+      return {
+        ...state,
+        isCorrect: action.payload
+      }
+    case 'isChecking':
+      return {
+        ...state,
+        isChecking: action.payload
+      }
+    case 'isLoading':
+      return {
+        ...state,
+        isLoading: false
+      }
+    case 'api':
+      return {
+        ...state,
+        api: action.payload
+      }
+    case 'textField':
+      return {
+        ...state,
+        textField: action.payload
+      }
+    default:
+      return initialState
+  }
+}
