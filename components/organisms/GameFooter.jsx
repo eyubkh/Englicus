d356a8be8582a4930974a8fd8e6a4b53499970b8
eyubkh <eyub.kh@gmail.com>
2022-11-04 18:20:26 +0000

    build: adding context and statement if is checking or isCorrect

--- components/organisms/GameFooter.jsx
+++ components/organisms/GameFooter.jsx
@@ -1,9 +1,19 @@
+import { useContext } from 'react'
+import { Context } from 'utils/context'
+import { GameFooterError } from './GameFooterError'
 import { GameFooterNeutral } from './GameFooterNeutral'
+import { GameFooterSuccess } from './GameFooterSuccess'
 
 export const GameFooter = () => {
-  return (
-    <>
-      <GameFooterNeutral />
-    </>
-  )
+  const state = useContext(Context)
+
+  if (state.isChecking && state.isCorrect) {
+    return <GameFooterSuccess />
+  }
+
+  if (state.isChecking && !state.isCorrect) {
+    return <GameFooterError />
+  }
+
+  return <GameFooterNeutral />
 }
