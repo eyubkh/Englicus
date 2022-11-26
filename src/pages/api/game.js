8f54b565a3cd302d49f15673a27d60eef84d3956
eyubkh <eyub.kh@gmail.com>
2022-11-26 15:10:59 +0000

    build: adding more challenges

--- src/pages/api/game.js
+++ src/pages/api/game.js
@@ -47,6 +47,150 @@ export default function handler (req, res) {
       ],
       correctIndex: 1,
       target: 'past'
+    },
+    {
+      translation: 'surgir',
+      choises: [
+        {
+          infinitive: 'arise'
+        },
+        {
+          past: 'arose'
+        },
+        {
+          participle: 'arisen'
+        }
+      ],
+      correctIndex: 1,
+      target: 'past'
+    },
+    {
+      translation: 'despertar(se)',
+      choises: [
+        {
+          infinitive: 'awake'
+        },
+        {
+          past: 'awoke'
+        },
+        {
+          participle: 'awoken'
+        }
+      ],
+      correctIndex: 1,
+      target: 'past'
+    },
+    {
+      translation: 'soportar',
+      choises: [
+        {
+          infinitive: 'bear'
+        },
+        {
+          past: 'bore'
+        },
+        {
+          participle: 'borne'
+        }
+      ],
+      correctIndex: 1,
+      target: 'past'
+    },
+    {
+      translation: 'surgir',
+      choises: [
+        {
+          infinitive: 'arise'
+        },
+        {
+          past: 'arose'
+        },
+        {
+          participle: 'arisen'
+        }
+      ],
+      correctIndex: 1,
+      target: 'past'
+    },
+    {
+      translation: 'despertar(se)',
+      choises: [
+        {
+          infinitive: 'awake'
+        },
+        {
+          past: 'awoke'
+        },
+        {
+          participle: 'awoken'
+        }
+      ],
+      correctIndex: 1,
+      target: 'past'
+    },
+    {
+      translation: 'soportar',
+      choises: [
+        {
+          infinitive: 'bear'
+        },
+        {
+          past: 'bore'
+        },
+        {
+          participle: 'borne'
+        }
+      ],
+      correctIndex: 1,
+      target: 'past'
+    },
+    {
+      translation: 'surgir',
+      choises: [
+        {
+          infinitive: 'arise'
+        },
+        {
+          past: 'arose'
+        },
+        {
+          participle: 'arisen'
+        }
+      ],
+      correctIndex: 1,
+      target: 'past'
+    },
+    {
+      translation: 'despertar(se)',
+      choises: [
+        {
+          infinitive: 'awake'
+        },
+        {
+          past: 'awoke'
+        },
+        {
+          participle: 'awoken'
+        }
+      ],
+      correctIndex: 1,
+      target: 'past'
+    },
+    {
+      translation: 'soportar',
+      choises: [
+        {
+          infinitive: 'bear'
+        },
+        {
+          past: 'bore'
+        },
+        {
+          participle: 'borne'
+        }
+      ],
+      correctIndex: 1,
+      target: 'past'
     }
   ]
 
