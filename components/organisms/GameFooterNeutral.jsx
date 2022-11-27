c06e678c2b9e1c866d0a4b94725b14e3d1994214
eyubkh <eyub.kh@gmail.com>
2022-11-27 18:27:13 +0000

    build: adding skip button logic

--- components/organisms/GameFooterNeutral.jsx
+++ components/organisms/GameFooterNeutral.jsx
@@ -30,7 +30,7 @@ export const GameFooterNeutral = () => {
   }
   return (
     <GameFooterNeutralComponent>
-      <SkipButton />
+      <SkipButton handler={handler} />
       <CheckButton handler={handler} />
     </GameFooterNeutralComponent>
   )
