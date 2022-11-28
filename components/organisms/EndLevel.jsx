32101db1b4ad0f19e0792c0017a282fbe1f39001
eyubkh <eyub.kh@gmail.com>
2022-11-28 20:18:25 +0000

    build: new end level design

--- components/organisms/EndLevel.jsx
+++ components/organisms/EndLevel.jsx
@@ -1,21 +1,38 @@
 import { Ad } from '@components/atoms/Ad'
-import { ProgressMade } from '@components/molecules/ProgressMade'
-import { Score } from '@components/molecules/Score'
+import Link from 'next/link'
+import { CrossIcon } from 'stories/atoms/CrossIcon.stories'
 import styled from 'styled-components'
+import { EndLevelFooter } from './EndLevelFooter'
 
 const EndLevelComponent = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: space-between;
   align-items: center;
   height: 100vh;
+  width: 100vw;
+  section {
+    display: grid; 
+    place-content: center;
+    width: 100%;
+    height: 100%;
+  }
 `
 
 export const EndLevel = () => {
   return (
     <EndLevelComponent>
-      <Score />
-      <ProgressMade reverse />
-      <Ad />
+      <section>
+        <div>
+          <Link href='/learn' passHref legacyBehavior>
+            <a>
+              <CrossIcon />
+            </a>
+          </Link>
+        </div>
+        <Ad />
+      </section>
+      <EndLevelFooter />
     </EndLevelComponent>
   )
 }
---
+++ components/organisms/EndLevelFooter.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import { GreenButton } from '@components/molecules/GreenButton'
+import { BorderButton } from '@components/molecules/BorderButton'
+import { BrandSecondary } from '@tokens'
+
+const EndLevelFooterComponent = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  min-height: 23vh;
+  padding: 20px 20%;
+  border-top: 4px solid ${BrandSecondary};
+`
+
+export const EndLevelFooter = () => {
+  return (
+    <EndLevelFooterComponent>
+      <BorderButton>
+        try again
+      </BorderButton>
+      <GreenButton>
+        Continue
+      </GreenButton>
+    </EndLevelFooterComponent>
+  )
+}
