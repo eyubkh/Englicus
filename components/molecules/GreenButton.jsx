4ffb1dfe6a62e573ccfca72deea0c4a7b80dc057
eyubkh <eyub.kh@gmail.com>
2022-11-28 20:17:51 +0000

    build: changing button succes and skip to greend and border

---
+++ components/molecules/GreenButton.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+import { Button } from '@components/atoms/Button'
+import { ActionSuccess200, ActionSuccess300, Border0, NeutralLight100 } from '@tokens'
+
+const GreenButtonComponent = styled(Button)`
+    color: ${NeutralLight100};
+    background-color: ${ActionSuccess200};
+    box-shadow: 0px ${Border0} 0px ${ActionSuccess300};
+    margin-top: 0px;
+    transition: box-shadow .1s, margin-top .1s;
+
+    :active {
+      margin-top: ${Border0};
+      box-shadow: 0px 0px 0px ${ActionSuccess300};
+    }
+`
+
+export const GreenButton = ({ handler, children }) => {
+  return <GreenButtonComponent onClick={handler}>{children}</GreenButtonComponent>
+}
