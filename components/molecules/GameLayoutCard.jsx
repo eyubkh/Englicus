c4c51134c2a2a5715b15245c54b5ff6cd8b18b99
eyubkh <eyub.kh@gmail.com>
2023-03-30 19:20:35 +0100

    build: adding style and refactor code

--- components/molecules/GameLayoutCard.jsx
+++ components/molecules/GameLayoutCard.jsx
@@ -1,21 +1,22 @@
-import { BodyH4, NeutralDark300, Radius2, Spacing0, Spacing2 } from '@tokens'
+import { BodyTextRegularBold, NeutralDark300, Radius2, Spacing0, Spacing2 } from '@tokens'
 import styled from 'styled-components'
 import Image from 'next/image'
 import Link from 'next/link'
 
 const GameLayoutCardComponent = styled.div`
-    display: inline-flex;
-    flex-direction: column;
-    text-align: center;
-    padding: ${Spacing2};
-    
+      display: inline-flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: ${Spacing2};
+
     img {
         border-bottom: ${Spacing0};
         border-radius: ${Radius2};
     }
 
-    a {
-        ${BodyH4}
+    p {
+        ${BodyTextRegularBold}
         margin: 0;
         color: ${NeutralDark300}
     }
@@ -23,11 +24,12 @@ const GameLayoutCardComponent = styled.div`
 
 export const GameLayoutCard = ({ src, alt, href, children }) => {
   return (
-    <GameLayoutCardComponent>
-      <Image src={src} alt={alt} width='240' height='120' />
-      <Link href={href}>
-        {children}
-      </Link>
-    </GameLayoutCardComponent>
+    <Link href={href}>
+      <GameLayoutCardComponent>
+        <Image src={src} alt={alt} width='240' height='120' />
+
+        <p>{children}</p>
+      </GameLayoutCardComponent>
+    </Link>
   )
 }
