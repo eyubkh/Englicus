bf8669196c48f307794dac6c936715c60031fa43
eyubkh <eyub.kh@gmail.com>
2023-03-30 19:21:04 +0100

    build: changing style and adding props

--- components/atoms/Logo.jsx
+++ components/atoms/Logo.jsx
@@ -1,21 +1,32 @@
 import styled from 'styled-components'
-import { BrandPrimary, NeutralLight100, BodyH4 } from '@tokens'
+import { BrandPrimary, NeutralLight100, BodyH4, NeutralDark100, Spacing2 } from '@tokens'
 import Link from 'next/link'
+import { homeHref } from '@utils/globalData'
 
 const LogoComponent = styled.h4`
+  position: absolute;
+  left: 0;
+  margin-bottom: ${Spacing2};
+
   ${BodyH4}
   cursor: pointer;
-  text-transform: uppercase;
-  color: ${NeutralLight100};
+  
   span {
     color: ${BrandPrimary};
   }
+  a {
+    text-transform: uppercase;
+    color: ${({ dark }) => dark ? NeutralDark100 : NeutralLight100};
+    text-decoration: none;
+  }
 `
 
-export const Logo = () => {
+export const Logo = ({ dark }) => {
   return (
-    <Link href='/'>
-      <LogoComponent><span>eng</span>licus</LogoComponent>
-    </Link>
+    <LogoComponent dark={dark}>
+      <Link href={homeHref}>
+        <span>eng</span>licus
+      </Link>
+    </LogoComponent>
   )
 }
