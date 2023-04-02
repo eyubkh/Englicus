9c9b4b2a5e71b48e8cd25a62003c150e5ff4fbed
eyubkh <eyub.kh@gmail.com>
2023-04-02 20:50:10 +0100

    build: adding useEffect, Loading and EndLesson com

--- components/pages/Lesson.jsx
+++ components/pages/Lesson.jsx
@@ -2,22 +2,35 @@ import { LessonFooter } from '@components/organisms/LessonFooter'
 import { LessonHeader } from '@components/organisms/LessonHeader'
 import { LessonSection } from '@components/organisms/LessonSection'
 import dataFetching from '@libs/dataFetching'
-import { LessonDispatch } from '@redux/LessonContext'
-import { useContext, useEffect } from 'react'
+import { handlerCleanUp, handlerEnterPress } from '@libs/handlerEventListener'
+import { LessonDispatch, LessonState } from '@redux/LessonContext'
+import { useContext, useEffect, useRef } from 'react'
+import { EndLesson } from './EndLesson'
+import { Loading } from './Loading'
 
 export const Lesson = () => {
   const dispatch = useContext(LessonDispatch)
+  const { isLoading, currentChallengeIndex, challenges } = useContext(LessonState)
+
+  const fetching = useRef(false)
 
   useEffect(() => {
+    if (fetching.current === true) return handlerCleanUp
+    fetching.current = true
+    handlerEnterPress()
+
     dataFetching('/api/game')
       .then((data) => {
-        console.log(data)
         dispatch({
           type: 'init',
           payload: data
         })
       })
   }, [])
+
+  if (isLoading) return <Loading />
+
+  if (currentChallengeIndex > challenges.length - 1) return <EndLesson />
 
   return (
     <>
