4d979eb1f2f5eaf06843e9c6890ae229b9e59973
eyubkh <eyub.kh@gmail.com>
2023-04-02 20:54:14 +0100

    build: new component EndLessonFooter

---
+++ components/organisms/EndLessonFooter.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import { GreenButton } from '@components/molecules/buttons/GreenButton'
+import { TransparentButton } from '@components/molecules/buttons/TransparentButton'
+import { BrandSecondary } from '@tokens'
+
+const EndLessonFooterComponent = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  min-height: 23vh;
+  padding: 20px 20%;
+  border-top: 4px solid ${BrandSecondary};
+`
+
+export const EndLessonFooter = () => {
+  return (
+    <EndLessonFooterComponent>
+      <TransparentButton>
+        try again
+      </TransparentButton>
+      <GreenButton>
+        Continue
+      </GreenButton>
+    </EndLessonFooterComponent>
+  )
+}
