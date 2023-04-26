fca912df49e38e483a6b0c5880ee29f3b222ee14
eyubkh <eyub.kh@gmail.com>
2023-04-26 08:10:23 +0100

    build: fixing error in speech

--- utils/textToSpeech.js
+++ utils/textToSpeech.js
@@ -1,5 +1,5 @@
 export function textToSpeech ({ value, lang = 'en-GB' }) {
-  const utterThis = new SpeechSynthesisUtterance(value)
+  const utterThis = new window.SpeechSynthesisUtterance(value)
   utterThis.lang = lang
 
   window.speechSynthesis.speak(utterThis)
