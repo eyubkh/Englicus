9ca4aa680a85daabe061474a1bb9c9a1ba0ce942
eyubkh <eyub.kh@gmail.com>
2024-01-18 10:12:45 +0000

    commit safety

--- apps/web/src/components/organisms/footers/WelcomeFooter.tsx
+++ apps/web/src/components/organisms/footers/WelcomeFooter.tsx
@@ -8,10 +8,18 @@ import { Button } from "@src/components/atoms/Button";
 //   min-height: 20vh;
 // `
 
-export default function WelcomeFooter() {
+export default function WelcomeFooter({
+	onClick = () => {},
+}: { onClick?: () => void }) {
 	return (
 		<footer className="flex justify-end">
-			<Button>Continuar</Button>
+			<button
+				className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800"
+				onClick={onClick}
+				type="button"
+			>
+				Continuar
+			</button>
 		</footer>
 	);
 }
--- apps/web/src/components/pages/Portal.tsx
+++ apps/web/src/components/pages/Portal.tsx
@@ -1,14 +1,42 @@
 import { PortalContextProvider } from "@src/redux/portal/portalContext";
 import { CrossedProgressBar } from "../molecules/CrossedProgressBar";
 import WelcomeFooter from "../organisms/footers/WelcomeFooter";
+import { useState } from "react";
+
+const data = [
+	{
+		prompt: "Que te motiva a aprender?",
+		options: ["trabajo", "viajar"],
+	},
+	{
+		prompt: "Elige tu nivel?",
+		options: [
+			"Aprendiento ingles por primeva vez?",
+			"Ya sebes algo de ingles?",
+		],
+	},
+];
 
 export default function Portal() {
+	const [progress, setProgress] = useState(0);
+	if (!data[progress]) {
+		return <h1>done</h1>;
+	}
 	return (
 		<PortalContextProvider>
 			<main className="flex flex-col justify-between h-full">
-				<CrossedProgressBar progress={50} />
-				<h1>hello</h1>
-				<WelcomeFooter />
+				<CrossedProgressBar progress={(progress / data.length) * 100} />
+				<div className="h-full flex flex-col justify-center p-10">
+					<h1 className="text-2xl font-bold">{data[progress].prompt}</h1>
+					{data[progress].options.map((o) => (
+						<p className="bg-slate-100 p-5 rounded-lg w-full mt-4">{o}</p>
+					))}
+				</div>
+				<WelcomeFooter
+					onClick={() => {
+						setProgress((old) => old + 1);
+					}}
+				/>
 			</main>
 		</PortalContextProvider>
 	);
