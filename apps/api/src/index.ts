f332546d8c6334f46e510f53ce2697365b287b42
eyubkh <eyub.kh@gmail.com>
2024-02-04 18:03:37 +0000

    safety commit

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,15 @@
 import { Hono } from "hono";
 import userRouter from "./model/user";
+require("dotenv").config();
+import mysql from "mysql2";
 
 const app = new Hono();
+
+const connection = mysql.createConnection(
+	`mysql://${process.env.PSCALE_DB_USERNAME}:${process.env.PSCALE_DB_PASSWORD}@aws.connect.psdb.cloud/englicus-db?ssl={"rejectUnauthorized":true}`,
+);
+console.log("Connected to PlanetScale!");
+connection.end();
 
 app.route("/test", userRouter);
 
